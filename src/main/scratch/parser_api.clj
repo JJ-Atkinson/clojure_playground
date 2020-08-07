e533b7054bd203d84d93352a94b59fa5c2313048
jarrett <jj_atkinson@att.net>
2020-08-07 17:32:51 -0500

    Convert the code to a more usable form

--- src/main/scratch/parser_api.clj
+++ src/main/scratch/parser_api.clj
@@ -1,106 +1,39 @@
-(ns quil.textract-pdf.parser
-  (:require [quil.core :as q]
-            [quil.running-app :as ra]
-            [meander.epsilon :as m]
+(ns scratch.parser-api
+  (:require [meander.epsilon :as m]
             [clojure.set :as set]
             [clojure.pprint :as pprint]
             [zprint.core :as zp]
             [medley.core :as mc]
-            [clojure.string :as str]))
-
-
-(defn integrate-bounding-box [x]
-  (merge x (-> x :Geometry :BoundingBox)))
-
-
-(defonce highlighted-ids (atom #{}))
-(def page (atom 2))
-(defn next-p! [] (swap! page inc))
-(defn prev-p! [] (swap! page dec))
-(comment (next-p!) (prev-p!))
-(def size-multiplier (atom 1900))
-(defonce selected-id (atom nil))
-(def conj-set (fnil conj #{}))
-
-(defn highlight-id! [id] (swap! highlighted-ids set/union (set (cond-> id (string? id) vector))))
-(defn unhighlight-id! [id] (swap! highlighted-ids disj id))
-(defn clear-highlights! [] (reset! highlighted-ids #{}))
-(comment
-  @highlighted-ids
-  (highlight-id! "id"))
-
-(comment (let [ids (m/search
-                     pdf-read
-                     (m/scan {:Id   ?id
-                              :Text (m/re #"Farm")})
-                     ?id)]
-           (highlight-id! ids))
-  (clear-highlights!))
-
-
-(do
-  (defn setup [& args]
-    (q/frame-rate 4)
-    (q/text-font (q/create-font "Ubuntu" 16)))
-  (reset! ra/sketch-setup setup))
-
-
-(defn distance [x1 y1 x2 y2]
-  (Math/sqrt (+ (Math/pow (- x1 x2) 2)
-               (Math/pow (- y1 y2) 2))))
-(comment (distance 1 1 2 2))
-
-(defn nearest-mouse [blocks]            ;; must have the position mounted top level
-  (let [mx (/ (q/mouse-x) @size-multiplier)
-        my (/ (q/mouse-y) @size-multiplier)]
-    (reduce (fn [[dist cblock] nblock]
-              (let [ndist (distance (:Left nblock) (:Top nblock) mx my)]
-                (if (< ndist dist)
-                  [ndist nblock]
-                  [dist cblock])))
-      [100 nil]
-      blocks)))
-
-
-
-
+            [clojure.string :as str]
+            [clojure.core.memoize :as mem]))
 
 
 
 (defn integrate-bounding-box [x]
   (merge x (-> x :Geometry :BoundingBox)))
 
-(def ^:dynamic *all-words* (->>
-                             ;"/home/jarrett/Downloads/blocks.edn"
-                             "/home/jarrett/code-projects/jra-aws/NewSite/cool-blocks.edn"
-                             slurp read-string (map integrate-bounding-box)))
-(def ^:dynamic *element-by-id* (into {} (map (fn [x] [(:Id x) x]) *all-words*)))
-
-(def + (fnil clojure.core/+ 0 0 0))
-(def - (fnil clojure.core/- 0 0 0))
-(def > (fnil clojure.core/> 0 0))
-(def < (fnil clojure.core/< 0 0))
+(def ^:dynamic *all-words* nil)
+(def ^:dynamic *element-by-id* nil)
 
 
 (def words-on-page
-  (memoize (fn [page-num]
-             (m/search *all-words*
-               (m/scan {:BlockType "WORD"
-                        :Page      ~page-num
-                        :as        ?q})
-               ?q))))
+  (mem/lu (fn [page-num]
+            (m/search *all-words*
+              (m/scan {:BlockType "WORD"
+                       :Page      ~page-num
+                       :as        ?q})
+              ?q))
+    :lu/threshold 50))
 
 
 (defn same?
   ([a b] (same? a b 0.002))
   ([a b tolerance]
-   (if (and a b)
-     (< (Math/abs (- a b)) tolerance)
-     false)))
-
+   (< (Math/abs (- a b)) tolerance)))
 (defn same-row?
   ([e1 e2] (same? (:Top e1) (:Top e2)))
   ([e1 e2 tolerance] (same? (:Top e1) (:Top e2) tolerance)))
+
 
 (defn close?
   ([origin] (close? origin 0.002))
@@ -110,9 +43,7 @@
 (defn below?
   ([origin] (below? origin 0.002))
   ([origin tolerance]
-   (fn [n] (if (and origin tolerance n)
-             (< (- origin tolerance) n)
-             false))))
+   (fn [n] (< (- origin tolerance) n))))
 
 (defn above?
   ([origin] (above? origin 0.002))
@@ -130,7 +61,6 @@
              :Top (m/pred (close? line-y))})
     ?id))
 
-
 (comment :highlight-all-on-selected-line
   (clear-highlights!)
   (->> @selected-id
@@ -139,22 +69,20 @@
     (on-line @page)
     highlight-id!))
 
-
 (defn farm-headings
   "Grabs all instances of \"Farm\" on the left of the page"
   [words]
   (m/search words
     (m/scan {:Id   ?id
              :Left (m/pred (close? 0 0.05))
-             :Text (m/re #"Far.*")})
+             :Text (m/re #"Farm")})
     ?id))
 
 (comment :highlight-farm-of-headers
   (clear-highlights!)
-  (->> pdf-read
+  (->> *all-words*
     farm-headings
     highlight-id!))
-
 
 (defn collect-headings [some-element]
   (on-line (:Page some-element) (:Top some-element)))
@@ -162,18 +90,17 @@
 
 (comment :highlight-headers
   (clear-highlights!)
-  (->> pdf-read
+  (->> *all-words*
     farm-headings
-    (map by-id)
+    (map *element-by-id*)
     (mapcat collect-headings)
     highlight-id!))
-
 
 (defn collect-left-bounds [header-ids]
   (let [initial-tag
         (map (fn [hid]
-               (let [tag (condp re-matches (or (:Text (get *element-by-id* hid)) "")
-                           #"Far.*" :farm
+               (let [tag (condp re-matches (:Text (get *element-by-id* hid))
+                           #"Farm" :farm
                            #"Tract" :tract
                            #"CLU.*" :clu-field
                            #"Irrigation.*" :irrigation-practice
@@ -215,17 +142,17 @@
       :planting-date (assoc planting-date :tag :planting-date)
       :planting-period (assoc planting-period :tag :planting-period))))
 
+
+
 (comment :grabbing-left-bounds
-  (->> pdf-read
+  (->> *all-words*
     farm-headings
-    first by-id
+    first *element-by-id*
     collect-headings
     collect-left-bounds
     (mc/map-vals :Left)
     seq
     (sort-by second)))
-
-
 
 
 (defn determine-y-locations
@@ -294,7 +221,7 @@
         ys (sort (map :Top (determine-y-locations CLU-header {:text-match? #(re-matches #"\d+.*" %)})))
         ordered-xs (sort-by :Left (vals tagged-headers))
         max-x 1
-        max-y (+ (or (last ys) 0) 0.025)
+        max-y (+ (last ys) 0.025)
         all-words-in
         (m/search (words-on-page (:Page some-header-block))
           (m/scan {:Left (m/and (m/pred (ahead? 0)) (m/pred (behind? 1)) ?left)
@@ -358,18 +285,6 @@
   (mmap-vals compact-words row-entities))
 
 
-
-(comment
-  (compact-words [{:Text "hi"}])
-  (compact-words [{:Text "hi" :Top 0 :Left 0}
-                  {:Text "hi2" :Top 0 :Left 2}])
-  (compact-words [{:Text "hi2" :Top 2 :Left 0}
-                  {:Text "hi" :Top 0 :Left 0}])
-  (compact-words [{:Text "hi2" :Top 2 :Left 0}
-                  {:Text "hi3" :Top 2 :Left 1}
-                  {:Text "hi" :Top 0 :Left 0}]))
-
-
 (def field-order [:farm
                   :tract
                   :clu-field
@@ -410,19 +325,18 @@
    The final result will be a map called :producers of share -> name"
   [r]
   (let [{ps :producer-share pn :producer-name} r
-        ps (str/split-lines (or ps ""))
-        pn (str/split-lines (or pn ""))
+        ps (str/split-lines ps)
+        pn (str/split-lines pn)
         producer-share-split (map (fn [ps]
                                     (let [[whole-match share owner-name]
                                           (re-matches #"(\d+\.\d{2})(?:\ ([\w\-]+))?" ps)]
-                                      [(Float/parseFloat (or share "0"))
+                                      [(Float/parseFloat share)
                                        owner-name])) ps)
         producers (into {} (map (fn [[share partial-name] rest-of-name]
                                   [share (str partial-name " " rest-of-name)])
                              producer-share-split
                              pn))]
-    (-> r (assoc :producers producers)
-      (update :fsa-physical-location (fnil str/replace "") "\n" " "))))
+    (assoc r :producers producers)))
 
 
 (comment :final-construction
@@ -441,19 +355,13 @@
 (defn parse-document []
   (map parse-table (farm-headings *all-words*)))
 
-
-
-
-
-
-
 (comment
   ;; parse the whole doc
   (mapcat identity (parse-document))
 
   ;; highlight tables
   (doall (map (fn [hid] (->> hid *element-by-id* tag-table (map :Id) highlight-id!))
-           (farm-headings pdf-read)))
+           (farm-headings *all-words*)))
 
   (spit "final-csv.csv"
     (str (str/join "\t" field-order) "\n"
@@ -461,85 +369,24 @@
                        (mapcat identity (parse-document)))))))
 
 
-
-
-(defn address-section-ids []
-  (m/search (words-on-page 1)
-    (m/scan {:Top  (m/and (m/pred (below? 0.07)) (m/pred (above? 0.14)))
-             :Left (m/pred (behind? 0.21))
-             :Id   ?id})
-    ?id))
-
-(comment (highlight-id! (address-section-ids)))
-
-
-(defn get-address []
-  (let [words (compact-words (map *element-by-id* (address-section-ids)))
-        [_ farm-name street city-abbr state zip]
-        (re-matches #"Producer Name and Address\n(.*)\n(\d+[\w+ ?]+)\n(.*), (\w\w) (\d+-\d+)"
-          words)]
-    {:farm-name farm-name :street street :city-abbr city-abbr :state state :zip zip}))
-
-(comment (println (get-address))
-  (re-matches #"Producer Name and Address\n(.*)\n(\d+[\w+ ?]+)\n(.*), (\w\w) (\d+-\d+)"
-    "Producer Name and Address\nKAL-MAC FARMS\n4690 SPRING PLACE CV E\nOLIVE BRANCH, MS 38654-8128"))
-
-
-(comment :final-construction
-  (->> @selected-id by-id tag-table tagged-table->entities compact-entities (map custom-row-correction)
-    (pprint/print-table field-order)))
+(defn parse-document []
+  (map parse-table (farm-headings *all-words*)))
 
 
 
+(defn parse-document2 [words]
+  (let [aw (map integrate-bounding-box words)
+        bid (into {} (map (fn [x] [(:Id x) x]) aw))]
+    (binding [*all-words* aw
+              *element-by-id* bid]
+      ;(println (type *element-by-id*))
+      ;(doall (map (fn [x] (->> (get *element-by-id* x)))
+      ;         (farm-headings *all-words*)))
+      ;(farm-headings *all-words*)
+      (doall (parse-document))
+      )))
 
 
 
-
-
-(defn draw-word [{:keys [Id Text Width Height Left Top custom-fill]}]
-  (q/fill 255)
-  (q/stroke 255)
-  (when (@highlighted-ids Id)
-    (q/fill 255 128 128))
-  (when custom-fill
-    (q/fill custom-fill))
-  (when Text
-    (q/text Text (* @size-multiplier Left) (* @size-multiplier Top))))
-
-
-(do (defn draw [& args]
-      (q/text-align :left :top)
-      (q/background 0)
-      (let [words (words-on-page @page)
-            [dist block] (nearest-mouse words)]
-        (doall (map draw-word words))
-        (q/text (str "Dist: " dist) (- (q/width) 120) 20)
-        (q/text (str "FPS: " (q/current-frame-rate)) (- (q/width) 120) 40)
-        (q/text (zp/zprint-str block 70) (- (q/width) 750) 20)
-        (when (q/mouse-pressed?)
-          (reset! selected-id (:Id block))
-          (q/background 255))
-        (draw-word (assoc block :custom-fill (q/color 128 255 128)))))
-    (reset! ra/sketch-draw draw))
-
-
-
-
-
-(defn mount-sketch []
-  (q/defsketch application
-    :title "PDF Debugger"
-    :setup @ra/sketch-setup
-    :draw (fn [& args] (apply @ra/sketch-draw args))
-    :key-typed (fn [] (case (q/key-as-keyword)
-                        :j (next-p!)
-                        :k (prev-p!)
-                        nil))
-    :size [1600 1500]
-    :features [:resizable])
-  (ra/mount-app! application))
-
-
-(comment (mount-sketch)
-
-  (q/available-fonts))
+(comment
+  (parse-document2 (->> "document-parsed-aws-textract.edn" slurp read-string)))
